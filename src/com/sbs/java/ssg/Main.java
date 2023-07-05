51fcb5f964416c043d961b6382ed551be3ddcef5
nowJun82 <oizill5481@icloud.com>
2023-07-05 12:24:33 +0900

    게시물 상세페이지 구현

--- src/com/sbs/java/ssg/Main.java
+++ src/com/sbs/java/ssg/Main.java
@@ -1,47 +1,97 @@
 package com.sbs.java.ssg;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-
-		System.out.println("=== 프로그램 시작 ===");
+		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
-		
+
 		int lastArticleId = 0;
-		
+		List<Article> articles = new ArrayList<>();
+
 		while (true) {
-			System.out.print("명령어 입력 : ");
+			System.out.printf("명령어) ");
 			String command = sc.nextLine();
 			command = command.trim();
-			System.out.printf("입력된 명령어 : %s\n", command);
-			
-			if ( command.length() == 0 ) {
+
+			if (command.length() == 0) {
 				continue;
 			}
-			
-			if ( command.equals("system exit") ) {
+
+			if (command.equals("system exit")) {
 				break;
-			}
-			else if ( command.equals("article write") ) {
-				int id = lastArticleId +1;
+			} else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
 				lastArticleId = id;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				
-				System.out.printf("%s번 글이 작성되었습니다.\n", id);
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+			} else if (command.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+
+				System.out.println("번호 | 제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+
+					System.out.printf("%3d, %s\n", article.id, article.title);
+				}
+			} else if (command.startsWith("article detail")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+
+				boolean found = false;
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						found = true;
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (found == false) {
+					System.out.printf("%d번 게시물은 존재하지 않는 게시물입니다.\n", id);
+					continue;
+				}
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", "2023-07-05");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
 			}
-			else if ( command.equals("article list") ) {
-				System.out.println("게시물이 없습니다.");
-			}
+
 			else {
-				System.out.printf("%s (은)는 존재하지 않는 명령어입니다.\n", command);
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
 		}
-		System.out.println("=== 프로그램 끝 ===");
-		
+
+		sc.close();
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
