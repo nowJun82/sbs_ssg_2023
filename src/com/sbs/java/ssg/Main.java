b651c221be42ee2cae2fd6855b1cb27ae53e0b79
nowJun82 <oizill5481@icloud.com>
2023-07-05 13:45:48 +0900

    게시물 작성 시간 구현

--- src/com/sbs/java/ssg/Main.java
+++ src/com/sbs/java/ssg/Main.java
@@ -30,8 +30,8 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-
-				Article article = new Article(id, title, body);
+				String regDate = Util.getNowDateStr();
+				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
@@ -67,7 +67,7 @@ public class Main {
 					continue;
 				}
 				System.out.printf("번호 : %d\n", foundArticle.id);
-				System.out.printf("날짜 : %s\n", "2023-07-05");
+				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 			} else if (command.startsWith("article delete")) {
@@ -104,11 +104,13 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 
-	public Article(int id, String title, String body) {
+	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
---
+++ src/com/sbs/java/ssg/Util.java
@@ -0,0 +1,16 @@
+package com.sbs.java.ssg;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+
+	public static String getNowDateStr() {
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date time = new Date();
+//		String time1 = format.format(time);
+		
+		return format.format(time);
+	}
+}
