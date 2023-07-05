b2cb63ab58a633bbe0f32534d46d02ec46634d45
nowJun82 <oizill5481@icloud.com>
2023-07-05 09:22:09 +0900

    게시물 작성 기능 구현 중

--- src/com/sbs/java/ssg/Main.java
+++ src/com/sbs/java/ssg/Main.java
@@ -9,17 +9,30 @@ public class Main {
 		System.out.println("=== 프로그램 시작 ===");
 		Scanner sc = new Scanner(System.in);
 		
+		int lastArticleId = 0;
+		
 		while (true) {
 			System.out.print("명령어 입력 : ");
 			String command = sc.nextLine();
 			command = command.trim();
+			System.out.printf("입력된 명령어 : %s\n", command);
 			
 			if ( command.length() == 0 ) {
 				continue;
 			}
 			
-			if ( command.equals("종료") ) {
+			if ( command.equals("system exit") ) {
 				break;
+			}
+			else if ( command.equals("article write") ) {
+				int id = lastArticleId +1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				System.out.printf("%s번 글이 작성되었습니다.\n", id);
 			}
 			else if ( command.equals("article list") ) {
 				System.out.println("게시물이 없습니다.");
@@ -28,8 +41,7 @@ public class Main {
 				System.out.printf("%s (은)는 존재하지 않는 명령어입니다.\n", command);
 			}
 		}
-//		System.out.printf("ID : %s\nPW : %s\n", ID, PW);
 		System.out.println("=== 프로그램 끝 ===");
-
+		
 	}
 }
