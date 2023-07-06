b17e97f19002b81d004fe1f288418d87513f609a
nowJun82 <oizill5481@icloud.com>
2023-07-06 09:44:14 +0900

    게시물 조회수 시스템 구현 및 기타

--- src/com/sbs/java/ssg/Main.java
+++ src/com/sbs/java/ssg/Main.java
@@ -42,12 +42,12 @@ public class Main {
 					continue;
 				}
 
-				System.out.println(" 번호 | 제목 ");
+				System.out.println(" 번호 | 조회 | 제목");
 
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 
-					System.out.printf("  %-2d | %s\n", article.id, article.title);
+					System.out.printf("%4d | %4d | %s\n", article.id, article.hit, article.title);
 				}
 
 			} else if (command.startsWith("article detail")) {
@@ -68,11 +68,14 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않는 게시물입니다.\n", id);
 					continue;
 				}
+				
+				foundArticle.increaseHit();
 
 				System.out.printf("번호 : %d\n", foundArticle.id);
 				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
+				System.out.printf("조회 : %s\n", foundArticle.hit);
 
 			} else if (command.startsWith("article modify")) {
 				String[] commandBits = command.split(" ");
@@ -139,11 +142,17 @@ class Article {
 	String regDate;
 	String title;
 	String body;
+	int hit;
 
 	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
 		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		this.hit = 0;
+	}
+	
+	public void increaseHit () {
+		hit++;
 	}
 }
